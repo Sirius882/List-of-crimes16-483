873c70a359c02bb23b3c855971c37030ada38d49
Sirius <172862565+Sirius882@users.noreply.github.com>
2025-05-27 20:46:44 +0800

    Update singalTh.py

--- singalTh.py
+++ singalTh.py
@@ -6,7 +6,7 @@ df = df.set_index(df.columns[0])
 
 j = 0
 for i in df.index:
-    prompt = "输出" + i + "的" + ','.join(df.columns) + "，用大写字母L分隔，如果没有就回答“无”。按顺序列出结论即可，不必列出项目名称。"
+    prompt = "输出" + i + "的" + ','.join(df.columns) + "，用大写字母S分隔，如果没有就回答“无”。按顺序列出结论即可，不必列出项目名称。"
 
     client = OpenAI(api_key="", base_url="https://api.deepseek.com")
     response = client.chat.completions.create(
@@ -17,7 +17,7 @@ for i in df.index:
         ],
         stream=False
     )
-    answer = response.choices[0].message.content.split('L')
+    answer = response.choices[0].message.content.split('S')
     df.loc[i] = answer
     
     # j += 1
